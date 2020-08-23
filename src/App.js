31af963ec6f5e494514427dfd0ca356db22546c2
faken09 <seb_jakobsen@live.dk>
2020-08-23 22:57:30 +0200

    rename intervalId to timeInterval

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
         'drag', 'loving'
     ])
 
+
     const [timeLeft, setTimeLeft] = useState( 10)
     useEffect(() => {
         // exit early when we reach 0
@@ -19,12 +20,12 @@ function App() {
 
         // save intervalId to clear the interval when the
         // component re-renders
-        const intervalId = setInterval(() => {
+        const timeInterval = setInterval(() => {
             setTimeLeft(timeLeft - 1);
         }, 1000);
 
         // clear interval on re-render to avoid memory leaks
-        return () => clearInterval(intervalId);
+        return () => clearInterval(timeInterval);
         // add timeLeft as a dependency to re-rerun the effect
         // when we update it
     }, [timeLeft]);
